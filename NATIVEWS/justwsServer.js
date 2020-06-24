697e9d6956ae7fdbcae934f0ebd9a22d11eaabe1
Atul Kumar <atul.kumar3@go-mmt.com>
2020-06-24 06:28:05 +0530

    Added native server

---
+++ NATIVEWS/justwsServer.js
@@ -0,0 +1,26 @@
+const http = require("http");
+
+const websocket = require("ws");
+
+const server = http.createServer((req, res) => {
+  res.end("I am up and running ");
+});
+
+const websocketServer = new websocket.Server({ server: server });
+
+websocketServer.on("headers", (headers, req) => {
+  console.log(headers);
+  // console.log(req);
+});
+
+websocketServer.on("connection", (ws, req) => {
+  ws.send("Hello atul");
+
+  ws.on("message", (msg) => {
+    console.log(msg);
+  });
+});
+
+server.listen(3031, () => {
+  console.log(" Data Upa nd unnin g");
+});
